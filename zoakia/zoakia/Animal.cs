8f41b2a3a28dd4a4d81dc347ad892f003fa05174
panagiotisak <93144597+panagiotisak@users.noreply.github.com>
2021-11-17 20:49:45 +0200

    Add files via upload

---
+++ zoakia/zoakia/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoakia
+{
+    class Animal
+    {
+        string name;
+        Boolean hasTail; 
+        void talk(string x)
+        {
+            Console.WriteLine(x);
+
+        }
+    }
+}
---
+++ zoakia/zoakia/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoakia
+{
+    class Cat
+    {
+        
+        void talk(string x)
+        {
+            Console.WriteLine(x);
+
+        }
+    }
+}
